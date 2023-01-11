87b494b8bb1b7633fcdaa43c975f548968b32e48
ghateprathamesh <ppg6@njit.edu>
2023-01-11 17:01:04 -0500

    update to portfolio

--- src/App.js
+++ src/App.js
@@ -8,11 +8,11 @@ function App() {
   return (
     <div className="App">
  <Container fluid>
-          <h1 className="display-3">WELCOME TO BOOTCAMP - Jan 10 2023 Edition</h1>
-          <p className="lead">My Name is Sachin Rajgire and I am instructor  </p>
-          <p className="lead"><a href="https://github.com/sachinrajgire">Here</a> is my Github Link  </p>
-          <p className="lead"><a href="http://webdevnotes.wynisco.com">Here</a> Here is Class Notes Link </p>
-          <p className="lead"><a href="http://webdevhw.wynisco.com">Here</a> Here is Homeworks Link </p>
+          <h1 className="display-3">WELCOME TO MY WYNSICO BOOTCAMP PAGE</h1>
+          <p className="lead">My Name is Prathamesh Ghate and I am a Master's student  </p>
+          <p className="lead"><a href="https://github.com/ghateprathamesh">Here</a> is my Github Link  </p>
+          <p className="lead"><a href="http://simp.ly/p/gb7l74">Here</a> Here is Class Notes Link </p>
+          <p className="lead"><a href="https://github.com/ghateprathamesh/front-end-homework">Here</a> Here is Homeworks Link </p>
           <p className="lead"><a href="https://docs.google.com/spreadsheets/d/1I75Kt78QSCRSAiF74yBttNyWVI4bq2ONsTWnN_ai9iA/edit?usp=sharing">Here</a> Main Google Spreadsheet </p>
 
      <DataTable />
