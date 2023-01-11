d42f93189f070f0ab3ac7d6c289e183ad04e5b2b
ghateprathamesh <ppg6@njit.edu>
2023-01-11 17:27:15 -0500

    links should open in new tab

--- src/App.js
+++ src/App.js
@@ -10,10 +10,10 @@ function App() {
  <Container fluid>
           <h1 className="display-3">WELCOME TO MY WYNSICO BOOTCAMP PAGE</h1>
           <p className="lead">My Name is Prathamesh Ghate and I am a Master's student  </p>
-          <p className="lead"><a href="https://github.com/ghateprathamesh">Here</a> is my Github Link  </p>
-          <p className="lead"><a href="http://simp.ly/p/gb7l74">Here</a> Here is Class Notes Link </p>
-          <p className="lead"><a href="https://github.com/ghateprathamesh/front-end-homework">Here</a> Here is Homeworks Link </p>
-          <p className="lead"><a href="https://docs.google.com/spreadsheets/d/1I75Kt78QSCRSAiF74yBttNyWVI4bq2ONsTWnN_ai9iA/edit?usp=sharing">Here</a> Main Google Spreadsheet </p>
+          <p className="lead"><a href="https://github.com/ghateprathamesh" target="_blank">Here</a> is my Github Link  </p>
+          <p className="lead"><a href="http://simp.ly/p/gb7l74" target="_blank">Here</a> Here is Class Notes Link </p>
+          <p className="lead"><a href="https://github.com/ghateprathamesh/front-end-homework" target="_blank">Here</a> Here is Homeworks Link </p>
+          <p className="lead"><a href="https://docs.google.com/spreadsheets/d/1I75Kt78QSCRSAiF74yBttNyWVI4bq2ONsTWnN_ai9iA/edit?usp=sharing" target="_blank">Here</a> Main Google Spreadsheet </p>
 
      <DataTable />
      </Container>
